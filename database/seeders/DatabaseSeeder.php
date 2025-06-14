19a468c892627244195fbd0adf40611c3b7cb5ee
Anhkato <anhttph48908@gmail.com>
2025-06-14 22:49:21 +0700

    pull cập nhật code từ nhánh demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         public function run()
         {
                 $this->call([
-                        // CategorySeeder::class,
+                        CategorySeeder::class,
                         // ProductSeeder::class,
                         // ProductVariantSeeder::class,
                         // ProductImageSeeder::class,
