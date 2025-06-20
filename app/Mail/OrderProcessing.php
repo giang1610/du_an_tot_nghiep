e0454e25f0f8cd3f55a5c522b2c06210e1f340a2
lyhongcong <conglhph48542@gmail.com>
2025-06-20 19:38:38 +0700

    1

---
+++ app/Mail/OrderProcessing.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
+
+class OrderProcessing extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    public function build()
+    {
+        return $this->subject('Đơn hàng của bạn đang được xử lý')
+                    ->markdown('emails.orders.processing');
+    }
+}
