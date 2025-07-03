286cb6a5302eb758d964fd074917e6f6dfa42b57
lyhongcong <conglhph48542@gmail.com>
2025-07-03 15:45:28 +0700

    acb

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -47,19 +47,21 @@ return [
         ],
 
         'pusher' => [
-        'driver' => 'pusher',
-        'key' => env('PUSHER_APP_KEY', 'local'),
-        'secret' => env('PUSHER_APP_SECRET', 'local'),
-        'app_id' => env('PUSHER_APP_ID', 'local'),
-        'options' => [
-        'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-        'useTLS' => false, // vì chạy local
-        'host' => '127.0.0.1',
-        'port' => 6001,
-        'scheme' => 'http',
-        ],
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY', 'local'),
+            'secret' => env('PUSHER_APP_SECRET', 'local'),
+            'app_id' => env('PUSHER_APP_ID', 'local'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
+                'useTLS' => false, // vì chạy local
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
             ],
-
+        ],
+        'log' => [
+            'driver' => 'log',
+        ],
 
     ]
 
