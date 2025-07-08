7c1c222710b7ba4e15ff132c4c4dca0f45099c87
vantu1221 <tunvph49703@gmail.com>
2025-07-08 13:39:39 +0700

    fix mua hàng

--- routes/api.php
+++ routes/api.php
@@ -111,7 +111,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 // Payment Momo
 Route::prefix('payment')->group(function () {
-    Route::post('/momo', [OrderController::class, 'payViaMomo'])->middleware('auth:sanctum');
+    Route::post('/momo', [OrderController::class, 'processMomoPayment'])->middleware('auth:sanctum');
     Route::post('/momo-notify', [OrderController::class, 'momoNotify']);
     // Route::get('/momo-return', [OrderController::class, 'momoReturn']);
     // Route::post('/orders/pay-momo', [OrderController::class, 'payViaMomo'])->middleware('auth:sanctum');
