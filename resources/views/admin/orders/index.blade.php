901610f3e7ec257fc407f81c50d55a3232ecf81b
mingne25 <minhntph48524@gmail.com>
2025-07-09 09:09:37 +0700

    09/07

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -112,7 +112,7 @@
                             <th>Sản phẩm</th>
                             <th>Địa chỉ</th>
                             <th>Tổng tiền</th>
-                            <th>Phương thức & Trạng thái</th>
+                            <th>PTTT & TTTT</th>
                             <th>TT giao hàng</th>
                             <th>Thao tác</th>
                         </tr>
@@ -160,7 +160,7 @@
                                     <div><i class="fas fa-map-marker-alt me-2"></i> {{ Str::limit($order->shipping_address, 3) }}</div>
                                 </div>
                             </td>
-                              <td class="text-end">
+                              <td>
                                 <strong>{{ number_format($order->total) }} VNĐ</strong>
                                 @if($order->discount > 0)
                                 <div class="text-danger small">
@@ -197,14 +197,14 @@
                                     @endif
                                 </div>
                             </td>
-                            <td>
+                            {{-- <td>
                                 <strong>{{ number_format($order->total) }} VNĐ</strong>
                                 @if($order->discount > 0)
                                 <div class="text-danger small">
                                     <i class="fas fa-tag me-1"></i> Giảm {{ number_format($order->discount) }}₫
                                 </div>
                                 @endif
-                            </td>
+                            </td> --}}
                             <td>
                                 @switch($order->status)
                                 @case('pending')
